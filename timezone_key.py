350fee4d9f23cce990717c47b6f9e88de7965507
Fain <71020731+Fain17@users.noreply.github.com>
2023-05-17 01:44:28 +0530

    Update timezone_key.py

--- timezone_key.py
+++ timezone_key.py
@@ -4,13 +4,19 @@ import random
 from datetime import datetime
 from pytz import all_timezones
 
+#Generating a randomized key
+
 def key_gen():
+      #choosing a random timezone 
       timeZ_rand = pytz.timezone(random.choice(all_timezones)) 
       
+      #getting the present time of that timezone
       dt_rand = datetime.now(timeZ_rand)
 
+      #converting the timezone into a float value
       timestamp = time.mktime(dt_rand.timetuple()) + dt_rand.microsecond/1e6
       
+      #Adding a random value between -10 billion to 10 billion 
       val = random.randrange(-1000000000,1000000000)
 
       timestamp+val
@@ -21,8 +27,5 @@ def key_gen():
     
       # Print the random number
       return rand_key
-
-key = key_gen()  
-print(key)
 
       
